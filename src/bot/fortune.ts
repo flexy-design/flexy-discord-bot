787e32394cfcac0e0edf447d9f4d3f2c781b0c59
hmmhmmhm <hmmhmmhm@naver.com>
2023-02-14 00:18:01 +0900

    feat: find 쿼리 테스트

--- src/bot/fortune.ts
+++ src/bot/fortune.ts
@@ -1,11 +1,25 @@
 import { Events } from "discord.js";
 import { houseCodeClient } from "../discord";
+import { env } from "../env";
+import { findHouseFortuneDate } from "../gql/findHouseFortuneDate";
 
 export const initializeFortuneBot = async () => {
-  houseCodeClient.on(Events.InteractionCreate, (interaction) => {
+  houseCodeClient.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
     // if (!interaction.isCommand()) return;
     if (interaction.commandName !== "운세") return;
+
+    const userId = interaction.user.id;
+
     interaction.reply("운세를 보여줄거에요 관리자님 어서 개발해주세요.");
+
+    const lastFoundedDate = await findHouseFortuneDate({
+      communityId: userId,
+      adminToken: env.cmsAdminToken,
+    });
+
+    console.log({
+      lastFoundedDate,
+    });
   });
 };
---
+++ src/gql/findHouseFortuneDate.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { endpoint } from "./constant";
+import { login } from "./login";
+
+export const findHouseHasntSentWelcomeQuery = `query ($communityId: String!) {
+  houseUserFortunes(where: {communityId: $communityId) {
+    id
+    lastDate
+    communityId
+  }
+}`;
+
+export const findHouseFortuneDate = async ({
+  communityId,
+  adminToken,
+}: {
+  communityId: string;
+  adminToken: string;
+}): Promise<{
+  type: "success" | "new";
+  lastDate?: string | null;
+}> => {
+  const [email, password] = adminToken.split(":");
+  const cookie = await login({
+    email,
+    password,
+  });
+
+  const { data: userData } = await axios.post(
+    endpoint,
+    {
+      query: findHouseHasntSentWelcomeQuery,
+      variables: {
+        communityId,
+      },
+    },
+    {
+      headers: {
+        cookie,
+      },
+    }
+  );
+  const foundedUserCommunityId = userData?.data?.communityUser?.communityId;
+  const foundedLastDate = userData?.data?.communityUser?.lastDate;
+
+  if (foundedUserCommunityId === communityId)
+    return { type: "success", lastDate: foundedLastDate };
+
+  return { type: "new" };
+};
