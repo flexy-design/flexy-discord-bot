e90ca26400f1adee7fc9694f63b00c223d04ccf7
hmmhmmhm <hmmhmmhm@naver.com>
2023-02-13 23:53:46 +0900

    feat: Update

--- src/discord.ts
+++ src/discord.ts
@@ -22,11 +22,19 @@ export const houseCodeClient = new Client({
 export const houseCodeCommands = new Collection();
 (client as any).commands = houseCodeCommands;
 
+const commandData = new SlashCommandBuilder()
+  .setName("운세")
+  .setDescription("하루 한 번 내 운세를 봅니다.");
+
 houseCodeCommands.set("운세", {
-  data: new SlashCommandBuilder()
-    .setName("운세")
-    .setDescription("하루 한 번 내 운세를 봅니다."),
+  data: commandData,
   async execute(interaction) {
     await interaction.reply("운세를 봅니다.");
   },
+});
+
+// enable command
+houseCodeClient.on("ready", async () => {
+  console.log("The client is ready!");
+  await houseCodeClient.application?.commands.create(commandData);
 });
