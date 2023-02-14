a98203490eb947175d66af8997dc60d46695adaf
hmmhmmhm <hmmhmmhm@naver.com>
2023-02-14 23:18:42 +0900

    fix: debug

--- src/shortUrl.ts
+++ src/shortUrl.ts
@@ -4,8 +4,10 @@ export const getShortUrl = async (url: string) => {
   const isGd = "https://is.gd/create.php?format=simple&url=";
   try {
     const { data: shortUrl } = await axios.get(isGd + url);
+    console.log({ shortUrl });
     return shortUrl;
   } catch (e) {
+    console.log(e);
     return url;
   }
 };
